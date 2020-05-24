a3f34fa40bd7df8dd863874ef3dbc419d372e45c
crycetruly <crycetruly@gmail.com>
2020-05-24 21:26:54 +0200

    ch(envs): update envs

--- expensesapi/settings.py
+++ expensesapi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't!nemaip3&gq76xx02o^j@xj*bvk!cy75@ybl6b=b^1zctj)3='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
